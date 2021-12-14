710dc1c9f7f6e78e0180200d791f7b4f09e5940a
Omega Mputu <kutekemeni93@gmail.com>
2021-12-14 10:18:29 +0100

    modal created and js file created

--- js/app.js
+++ js/app.js
@@ -1,3 +1,76 @@
 (function (){
-    
+    var $modal = $('#modal');
+    var image = document.getElementById('sample_image');
+    var cropper;
+
+    $('#upload_image').change(function(event){
+        var files = event.target.files;
+
+        var done = function(url){
+            image.src = url;
+            $modal.modal('show');
+        };
+
+        var file;
+        var reader;
+
+        if(files && files.length > 0)
+        {
+            file = files[0];
+            reader = new FileReader();
+            reader.onload = function(event)
+            {
+                done(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    $modal.on('shown.bs.modal', function() {
+        cropper = new Cropper(image, {
+            aspectRatio: 1,
+            viewMode: 3,
+            preview:'.preview'
+        });
+    }).on('hidden.bs.modal', function(){
+        cropper.destroy();
+        cropper = null;
+    });
+
+    $('#crop').click(function(){
+
+        canvas = cropper.getCroppedCanvas({
+            width:140,
+            height:140
+        });
+
+        canvas.toBlob(function(blob){
+            url = URL.createObjectURL(blob);
+            var reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = function(){
+
+                var base64data = reader.result;
+                $.ajax({
+                    method: "POST",
+                    url: "{{ route('crop-image-upload') }}",
+                    dataType: 'JSON',
+                    data: {
+                        '_token': $('meta[name="csrf-token"]').attr('content'),
+                        'profile_photo': base64data,
+                        '_method': 'PUT'
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        $modal.modal('hide');
+                        //alert("Crop image successfully uploaded");
+                    },
+                    error: function (data) {
+                        console.log('Error');
+                        console.log(data);
+                    }
+                });
+            };
+        });
+    });
 })();
